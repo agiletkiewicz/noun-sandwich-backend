6c08d65e1c9943857ca450a2db7e2a025a16e2da
agiletkiewicz <a.letkiewicz@gmail.com>
2020-12-09 22:31:26 -0800

    add player model and table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_10_062813) do
+ActiveRecord::Schema.define(version: 2020_12_10_063059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,4 +22,13 @@ ActiveRecord::Schema.define(version: 2020_12_10_062813) do
     t.integer "team_two_points"
   end
 
+  create_table "players", force: :cascade do |t|
+    t.string "name"
+    t.bigint "game_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["game_id"], name: "index_players_on_game_id"
+  end
+
+  add_foreign_key "players", "games"
 end
